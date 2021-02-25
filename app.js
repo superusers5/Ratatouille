83f32f597c42079e0ec146b72fe92c938689da1c
superusers5 <superusers5hci@gmail.com>
2021-02-25 08:58:56 +0530

    updated

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const exphbs = require("express-handlebars");
 const methodOverride = require('method-override')
 const passport = require("passport");
 const session = require("express-session");
-// const MongoStore = require('connect-mongo')(session);
 const connectDB = require("./config/db");
 
 // Load config
@@ -73,7 +72,6 @@ app.use(
         secret: "keyboard cat",
         resave: false,
         saveUninitialized: false,
-        // store: new MongoStore({ mongooseConnection: mongoose.connection }),
     })
 );
 
