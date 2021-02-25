5efbaea292b3ff501bd8d2fce15b597cbf76e115
Thakur Shivank Singh <57209884+Shivankthakur@users.noreply.github.com>
2021-02-26 04:30:21 +0530

    config and code fixes (#6)
    * config
    
    * config
    
    * config changes
    
    * fixed UI textbox bug
    
    * config
    
    Co-authored-by: superusers5 <superusers5hci@gmail.com>

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
-const methodOverride = require('method-override')
+const methodOverride = require("method-override");
 const passport = require("passport");
 const session = require("express-session");
 const connectDB = require("./config/db");
@@ -20,20 +20,20 @@ connectDB();
 const app = express();
 
 // Body parser => to accept form data and json data
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Method override
 app.use(
-  methodOverride(function (req, res) {
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-      // look in urlencoded POST bodies and delete it
-      let method = req.body._method
-      delete req.body._method
-      return method
-    }
-  })
-)
+    methodOverride(function (req, res) {
+        if (req.body && typeof req.body === "object" && "_method" in req.body) {
+            // look in urlencoded POST bodies and delete it
+            let method = req.body._method;
+            delete req.body._method;
+            return method;
+        }
+    })
+);
 
 // Logging
 if (process.env.NODE_ENV === "development") {
@@ -41,24 +41,16 @@ if (process.env.NODE_ENV === "development") {
 }
 
 // Handlebars Helpers
-const {
-  formatDate,
-  stripTags,
-//   truncate,
-//   editIcon,
-  select,
-} = require('./helpers/hbs')
+const { formatDate, stripTags, select } = require("./helpers/hbs");
 
 // Handlebars
 app.engine(
     ".hbs",
     exphbs({
         helpers: {
-          formatDate,
-          stripTags,
-        //   truncate,
-        //   editIcon,
-          select,
+            formatDate,
+            stripTags,
+            select,
         },
         defaultLayout: "main",
         extname: ".hbs",
@@ -81,9 +73,9 @@ app.use(passport.session());
 
 // Set global var => allow us access to user from within our template
 app.use(function (req, res, next) {
-  res.locals.user = req.user || null
-  next()
-})
+    res.locals.user = req.user || null;
+    next();
+});
 
 // Static folder
 app.use(express.static(path.join(__dirname, "public")));
@@ -91,7 +83,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Routes
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
-app.use('/menus', require('./routes/menus'))
+app.use("/menus", require("./routes/menus"));
 
 const PORT = process.env.PORT || 3000;
 
